c21bb79f224b107891f643351df129573a42d317
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2021-01-10 17:47:10 +0530

    Add files via upload

---
+++ 2021/January/1/10-Jan/Alternate memoize as nested functions.py
@@ -0,0 +1,22 @@
+#note that this decorator ignores **kwargs
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        if args not in cache:
+            cache[args] = obj(*args, **kwargs)
+        return cache[args]
+    return memoizer
+
+#Here's a modified version that also respects kwargs
+def memoize(obj):
+    cace = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
