fadf300fdd8ac100fbda68b55af6c2cb8eeb1b7f
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2021-01-14 17:08:56 +0530

    Add files via upload

---
+++ 2021/January/1/14-Jan/Ignoring Deprecation Warnings.py
@@ -0,0 +1,26 @@
+import warnings
+
+def ignore_deprecation_warnings(func):
+    '''This is a decorator which can be used to ignore deprecation warnings
+    occuring in a function.'''
+    def new_func(*args,**kwargs):
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore",category=DeprecationWarning)
+            return func(*args,**kwargs)
+    new_func.__name__=func.__name__
+    new_func.__doc__=func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
+
+#Example of use
+
+@ignore_deprecation_warnings
+def some_function_raising_deprecation_warninig():
+    warnings.warn("This is a deprecationg warning.",
+                  category=DeprecationWarning)
+
+class SomeClass:
+    @ignore_deprecation_warninigs
+    def some_method_raising_deprecation_warning():
+        warnings.warn("This is a deprecationg warning.",
+                      category=DeprecationWarning)
